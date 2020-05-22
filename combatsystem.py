ff4d6f36cec02992679d3350193b6e972c92786e
Arivald8 <59714150+Arivald8@users.noreply.github.com>
2020-05-22 23:22:43 +0100

    Update combatsystem.py
    Annotated the functions with comments.

--- combatsystem.py
+++ combatsystem.py
@@ -3,47 +3,62 @@ import Monster_Hunter as mh
 import time
 import sys
 
+
 def dice_roll():
+    # Simple dice roll function
     dice_number = random.randrange(1, 11)
     return dice_number
 
 
 def attack():
+    """
+    This function defines the strength of the player hit. It does so by multiplying player strength by the attack
+    statistic of the currently equipped weapon. It then multiplies that by the dice_dict value of the player_roll.
+    """
     dice_dict = {1: 0.1, 2: 0.2, 3: 0.3, 4: 0.4, 5: 0.5, 6: 0.6, 7: 0.7, 8: 0.8, 9: 0.9, 10: 1}
+
     player_roll = dice_roll()
     print(f"You rolled {player_roll} \n")
-    time.sleep(2)
+    # time.sleep(2)
 
-    player_hit = mh.player.player_damage * mh.player.equipped_items["Weapon"].item_attack * dice_dict[player_roll]
+    player_hit = mh.player.player_strength * mh.player.equipped_items["Weapon"].item_attack * dice_dict[player_roll]
     return player_hit
 
 
 def deal_dmg(some_monster, player_hit):
+    # The only purpose of this function is to deal damage to some_monster and to print the result.
     some_monster.monster_hp = some_monster.monster_hp - player_hit
     print(f"You hit {some_monster.monster_name} for {player_hit} damage... {some_monster.monster_hp}hp remaining \n")
-    time.sleep(4)
+    # time.sleep(4)
 
 
 def monster_attack(monster):
+    # This function is the same as the attack(), but it is used to perform attacks by monsters.
     dice_dict = {1: 0.1, 2: 0.2, 3: 0.3, 4: 0.4, 5: 0.5, 6: 0.6, 7: 0.7, 8: 0.8, 9: 0.9, 10: 1}
+
     monster_roll = dice_roll()
     print(f"The monster rolled {monster_roll} \n")
-    time.sleep(2)
+    # time.sleep(2)
 
     monster_hit = monster.monster_attack * dice_dict[monster_roll]
     mh.player.player_hp = mh.player.player_hp - monster_hit
     print(f"You get hit for {monster_hit} damage... {mh.player.player_hp}hp remaining\n")
-    time.sleep(4)
+    # time.sleep(4)
 
 
 def battle(monster):
-    while monster.monster_hp > 0 and mh.player.player_hp > 0:
-        fight = True
-        while fight:
+    """
+    This function is the core of the combat system. Whenever a fight is to occur, this function gets called.
+    As long as the monster or the player are above 0hp, it will keep asking the player to roll the dice or to flee.
+    """
+    fight = True
+    while fight:
+        while monster.monster_hp > 0 and mh.player.player_hp > 0:
+
             action = input("\nroll/flee? > ")
             if action == "roll":
                 print("\nIt's your turn to roll the dice... \n")
-                time.sleep(2)
+                # time.sleep(2)
                 player_hit = attack()
                 deal_dmg(monster, player_hit)
                 monster_attack(monster)
@@ -59,21 +74,5 @@ def battle(monster):
             else:
                 print("Unknown command...")
 
-
-
-
-
-    print("Monster defeated.")
-"""
-roll dice for player
-
-Take player attack value * his weapon attack value and store as X
-
-Take monster defence store as Y
-
-Y - X = hit
-
-if you roll 10, it hits with 100% potential
-
-if you roll 1, it hits with 1% potential
-"""
+        print("Monster defeated.")
+        fight = False
