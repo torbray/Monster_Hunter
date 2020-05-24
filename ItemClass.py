23d1224b5bcbf3d4fe4d02f0ddf5b7b3d8ebdf9f
Arivald8 <59714150+Arivald8@users.noreply.github.com>
2020-05-24 19:35:36 +0100

    Update ItemClass.py

--- ItemClass.py
+++ ItemClass.py
@@ -1,17 +1,38 @@
+import PlayerClass
+
+
 class Item:
     # The item class defines an item name, type, whether it is hidden on the board and its position
 
     def __repr__(self):
         return self.name
 
-    def __init__(self, name, i_type, hidden, position, attack, defence):
+    def __init__(self, name, i_type, hidden, position, damage, defence):
         self.name = name
         self.i_type = i_type
         self.hidden = hidden
         self.position = position
-        self.attack = attack
+        self.damage = damage
         self.defence = defence
 
 
-# Create an item a sword and place it in player inventory
-wooden_stick = Item("Wooden stick", "Sword", " ", None, 5, 0)
+def showStats():
+    item = input("Lookup item stats for: ").lower()
+    while True:
+        if item in item_stats:
+            if item in PlayerClass.char.equipped_items or item in PlayerClass.char.inventory:
+                print(f"\n{item_stats[item][0]}: Damage: {item_stats[item][1]} Defence: {item_stats[item][2]}")
+                break
+        else:
+            print("Invalid item.")
+            break
+
+
+# Create items
+wooden_stick = Item("Wooden Stick", "Sword", " ", None, 5, 0)
+wooden_shield = Item("Wooden Shield", "Shield", " ", None, 0, 5)
+
+item_stats = {
+    "wooden stick": [wooden_stick.name, wooden_stick.damage, wooden_stick.defence],
+    # "wooden shield": [wooden_shield.name, wooden_shield.damage, wooden_shield.defence]
+}
