9dbbb971ecf364d2431b02c6bb5d6edeca3c4b59
Arivald8 <59714150+Arivald8@users.noreply.github.com>
2020-05-25 14:16:47 +0100

    Update PlayerClass.py

--- PlayerClass.py
+++ PlayerClass.py
@@ -46,5 +46,44 @@ class Player:
         print("---------------------")
         print(f"Gold: {self.gold}")
 
+    def showStats(self):
+        print("-----------------------")
+        print("--Character Statistic--")
+        print("-----------------------")
+        print(f"Your base Health: {self.hp}")
+        print(f"Your base Attack: {self.strength}")
+        print(f"Your base Defence: {self.defence}")
+        print("-----------------------")
+
+        # Calculate defence and damage stats from items
+        shield = self.equipped_items['Shield']
+        try:
+            shield_def = shield.defence
+        except AttributeError:
+            shield_def = 0
+
+        helmet = self.equipped_items['Helmet']
+        try:
+            helmet_def = helmet.defence
+        except AttributeError:
+            helmet_def = 0
+
+        chest = self.equipped_items['Chest']
+        try:
+            chest_def = chest.defence
+        except AttributeError:
+            chest_def = 0
+
+        weapon = self.equipped_items['Weapon']
+        try:
+            weapon_dmg = weapon.damage
+        except AttributeError:
+            weapon_dmg = 0
+
+        print(f"Your Total Health: {self.hp}")
+        print(f"Your Total Strength: {self.strength + weapon_dmg}")
+        print(f"Your Total Defence: {self.defence + shield_def + helmet_def + chest_def}")
+        print("-----------------------")
+
 
 char = Player("P", 0, 100, 3, 1)
