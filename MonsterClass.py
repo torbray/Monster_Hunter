e3dad92e8807f861e9a5fc0a6632142c9e6d5e8a
Arivald8 <59714150+Arivald8@users.noreply.github.com>
2020-05-25 21:23:52 +0100

    Update MonsterClass.py

--- MonsterClass.py
+++ MonsterClass.py
@@ -4,7 +4,7 @@ import random
 class Monster:
     # The monster class defines a name, position and whether the monster is currently hidden on the board
 
-    def __init__(self, name, symbol, position, hidden, found, hp, attack, defence, defeated):
+    def __init__(self, name, symbol, position, hidden, found, hp, attack, defence, defeated, gold_loot):
         self.name = name
         self.symbol = symbol
         self.position = position
@@ -14,6 +14,13 @@ class Monster:
         self.attack = attack
         self.defence = defence
         self.defeated = defeated
+        self.gold_loot = gold_loot
+
+
+# Generate random amount of gold for orcs
+def gen_orc_gold():
+    random_gold = random.randrange(10, 31)
+    return random_gold
 
 
 # Generate a random position for the monsters
@@ -23,6 +30,6 @@ def gen_ran_pos():
 
 
 # Create 10 orcs on the board
-army_of_orcs = [Monster("Bald Orc", "m", gen_ran_pos(), " ", False, 100, 11, 2, False) for _ in range(10)]
+army_of_orcs = [Monster("Bald Orc", "m", gen_ran_pos(), " ", False, 100, 11, 2, False, gen_orc_gold()) for _ in range(10)]
 
-orc_boss = Monster("Destroyer Orc", "M", gen_ran_pos(), " ", False, 500, 15, 8, False)
+orc_boss = Monster("Destroyer Orc", "M", gen_ran_pos(), " ", False, 500, 15, 8, False, 500)
