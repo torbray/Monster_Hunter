ae7ac3b3cd16809d9b49d075b543e3d821d4d0f9
Arivald8 <59714150+Arivald8@users.noreply.github.com>
2020-05-27 20:29:24 +0100

    Second try (#16)
    I don't know where the other fork went. Learning stuff with GitHub!
    
    This appears to print the same as your other code - hopefully anyway
    
    Co-authored-by: torbray <66016078+torbray@users.noreply.github.com>

--- GameBoard.py
+++ GameBoard.py
@@ -4,54 +4,21 @@ def draw_board(board):
     This function also checks whether the player encountered a monster and if he did, it changes the position of the
     monster from hidden to monster_name.
     """
+    line = '----------------------------------------'
+    
+    print(f'{line}\n{line}', end='')
 
-    print('----------------------------------------')
-    print('----------------------------------------')
-    print('----------------------------------------')
-    print(
-        f' {board[90]} | {board[91]} | {board[92]} | {board[93]} | {board[94]} | {board[95]} | {board[96]} | {board[97]} | {board[98]} | {board[99]} |')
-    print('----------------------------------------')
-    print(
-        f' {board[80]} | {board[81]} | {board[82]} | {board[83]} | {board[84]} | {board[85]} | {board[86]} | {board[87]} | {board[88]} | {board[89]} |')
-    print('----------------------------------------')
-    print(
-        f' {board[70]} | {board[71]} | {board[72]} | {board[73]} | {board[74]} | {board[75]} | {board[76]} | {board[77]} | {board[78]} | {board[79]} |')
-    print('----------------------------------------')
-    print(
-        f' {board[60]} | {board[61]} | {board[62]} | {board[63]} | {board[64]} | {board[65]} | {board[66]} | {board[67]} | {board[68]} | {board[69]} |')
-    print('----------------------------------------')
-    print(
-        f' {board[50]} | {board[51]} | {board[52]} | {board[53]} | {board[54]} | {board[55]} | {board[56]} | {board[57]} | {board[58]} | {board[59]} |')
-    print('----------------------------------------')
-    print(
-        f' {board[40]} | {board[41]} | {board[42]} | {board[43]} | {board[44]} | {board[45]} | {board[46]} | {board[47]} | {board[48]} | {board[49]} |')
-    print('----------------------------------------')
-    print(
-        f' {board[30]} | {board[31]} | {board[32]} | {board[33]} | {board[34]} | {board[35]} | {board[36]} | {board[37]} | {board[38]} | {board[39]} |')
-    print('----------------------------------------')
-    print(
-        f' {board[20]} | {board[21]} | {board[22]} | {board[23]} | {board[24]} | {board[25]} | {board[26]} | {board[27]} | {board[28]} | {board[29]} |')
-    print('----------------------------------------')
-    print(
-        f' {board[10]} | {board[11]} | {board[12]} | {board[13]} | {board[14]} | {board[15]} | {board[16]} | {board[17]} | {board[18]} | {board[19]} |')
-    print('----------------------------------------')
-    print(
-        f' {board[0]} | {board[1]} | {board[2]} | {board[3]} | {board[4]} | {board[5]} | {board[6]} | {board[7]} | {board[8]} | {board[9]} |')
-    print('----------------------------------------')
-    print('----------------------------------------')
-    print('----------------------------------------')
+    # defines the row in 10s, goes from 90 to 0
+    for row in range(9, -1, -1):
+        print(f'\n{line}')
+
+        # counts in 1s e.g. 90 to 90 + 9
+        for pos in range(row*10, row*10 + 10):
+            # ' B |' + ' X |' = ' B | X |'
+            print(f' {board[pos]} |', end='')
+            
+    print(f'\n{line}\n{line}\n{line}')
 
 
 # --- Create the board --- #
-theBoard = [
-    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-    " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-    " ", " ", " ", " ", " ", " ", " ", " ", " ", " "
-]
+theBoard = [' ' for i in range(0, 100)]
